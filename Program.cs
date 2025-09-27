1c9463c6ab75ab619dad80d85f437ff97944bb26
cuongpham87 <lil.steven296@gmail.com>
2025-09-27 23:51:33 +0630

    add
    add

--- Program.cs
+++ Program.cs
@@ -1,27 +1,10 @@
-﻿class Program{
+﻿class Program {
     public static void Main(string[] args)
     {
         int age;
         Console.WriteLine("Nhập tuổi của bạn");
         age = Convert.ToInt32(Console.ReadLine());
-        string status;
-        if (age >= 18)
-        {
-            status = "adult";
-            Console.WriteLine("bạn đã trưởng thành");
-
-        }
-        else if (age < 18 && age >= 13)
-        {
-            status = "teenager";
-            Console.WriteLine("Bạn là thanh thiếu niên");
-        }
-        else
-        {
-            status = "child";
-            Console.WriteLine("bạn là trẻ em"); 
-        }
-
-    
+        string result = (age >= 18) ? "trưởng thành" : (age >= 13 ? "vị thành niên" : "trẻ em");
+        Console.WriteLine(result);
     }
 }
